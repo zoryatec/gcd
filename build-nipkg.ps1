c873e600207ee80ed0a546b0ba5ce26f10c1309d
Dawid Wozny (DEV) <dawid.wozny@zoryatec.com>
2024-11-28 18:14:47 +0000

    adding build nipkg

---
+++ build-nipkg.ps1
@@ -0,0 +1,20 @@
+﻿
+
+
+function UpdateCtrFile{
+    param([string] $ctrFilePath = ".\package\control\control", [string] $shortVersion = "0.2.0.1" , $buildNumber )
+
+
+    $versionLine = "Version: "+$shortVersion+"-"+$buildNumber
+    $versionLine
+    $ctrFile = Get-Content $ctrFilePath
+    $ctrFil = $ctrFile + $versionLine
+    $ctrFil
+    Set-Content -Path $ctrFilePath -Value $ctrFil
+}
+
+
+UpdateCtrFile -ctrFilePath ".\package\control\control" -shortVersion "0.3.0.1" -buildNumber 3
+
+
+nipkg pack ".\package" ".\package"
