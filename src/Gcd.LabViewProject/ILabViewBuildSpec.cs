e7477a0eb9260ab98ec759fc6faf0f6ba435b891
Dawid Wozny <dawid.wozny@zoryatec.com>
2024-11-30 11:01:24 +0100

    getting real with lvproj

--- src/Gcd.LabViewProject/ILabViewBuildSpec.cs
+++ src/Gcd.LabViewProject/ILabViewBuildSpec.cs
@@ -2,5 +2,6 @@ namespace Gcd.LabViewProject;
 
 public interface ILabViewBuildSpec
 {
-    public void SetVersion(BuildSpecVersion version);
+    public string Name { get; set; }
+    public string Type { get; set; }
 }
--- src/Gcd.LabViewProject/LabViewProject.cs
+++ src/Gcd.LabViewProject/LabViewProject.cs
@@ -1,16 +1,42 @@
+using System.Collections.Immutable;
+using System.Xml;
 using CSharpFunctionalExtensions;
 
 namespace Gcd.LabViewProject;
 
 public class LabViewProject
 {
-    public static Result<LabViewProject> Create(string pathToProject)
+    private string _fileContent;
+    private readonly List<LabViewBuildSpec> _buildSpecifications;
+    private List<LabViewBuildSpec> _buildSpecsCopy;
+
+    public static Result<LabViewProject> Create(string fileContent)
     {
-        return new LabViewProject();
+        return new LabViewProject(fileContent);
     }
     
-    private LabViewProject()
+    private LabViewProject(string fileContent)
     {
-        
+        _fileContent = fileContent;
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(_fileContent);
+
+        XmlNode buildSpecsNode = doc.SelectSingleNode("//Item[@Name='Build Specifications' and @Type='Build']");
+
+        _buildSpecifications = new List<LabViewBuildSpec>();
+        // Extract and print all build specification names
+        foreach (XmlNode item in buildSpecsNode.SelectNodes("Item"))
+        {
+            string innerText = item.InnerText;
+            var buildSpec = LabViewBuildSpec.Create(item);
+            string name = buildSpec.GetName();
+            string type = buildSpec.GetType();
+            _buildSpecifications.Add(buildSpec);
+        }
+    }
+
+    public List<LabViewBuildSpec> BuildSpecifications
+    {
+        get => _buildSpecifications;
     }
 }
