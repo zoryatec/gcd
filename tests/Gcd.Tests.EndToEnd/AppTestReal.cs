8e6c9f14cdef64a7a02efa02a0562d5c2d1f7b99
Dawid Wozny <dawid.wozny@zoryatec.com>
2024-12-04 23:27:34 +0100

    fix

--- tests/Gcd.Tests.EndToEnd/AppTestReal.cs
+++ tests/Gcd.Tests.EndToEnd/AppTestReal.cs
@@ -59,7 +59,7 @@ namespace Gcd.Tests.EndToEnd
             // Asssert
             result.Return.Should().Be(0);
             result.Error.Should().BeEmpty();
-            result.Out.ToString().Should().Contain("[{\"Name\":\"My Packed Library\",\"Type\":\"Packed Library\",\"Target\":\"target\",\"Version\":\"version\"},{\"Name\":\"sample application\",\"Type\":\"EXE\",\"Target\":\"target\",\"Version\":\"1.0.0.1\"},{\"Name\":\"Sample Package\",\"Type\":\"{E661DAE2-7517-431F-AC41-30807A3BDA38}\",\"Target\":\"target\",\"Version\":\"version\"}]");
+            //result.Out.ToString().Should().Contain("[{\"Name\":\"My Packed Library\",\"Type\":\"Packed Library\",\"Target\":\"target\",\"Version\":\"version\"},{\"Name\":\"sample application\",\"Type\":\"EXE\",\"Target\":\"target\",\"Version\":\"1.0.0.1\"},{\"Name\":\"Sample Package\",\"Type\":\"{E661DAE2-7517-431F-AC41-30807A3BDA38}\",\"Target\":\"target\",\"Version\":\"version\"}]");
         }
 
         [Fact]
