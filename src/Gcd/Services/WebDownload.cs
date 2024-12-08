0126f521977343107c3af0c0f2ea9bfc2a2c473c
Dawid Wozny <dawid.wozny@zoryatec.com>
2024-12-08 13:49:08 +0100

    fix for now

---
+++ src/Gcd/Services/WebDownload.cs
@@ -0,0 +1,24 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gcd.Services
+{
+    public class WebDownload : IWebDownload
+    {
+        public async Task<Result> DownloadFileAsync(WebUri webUri, FilePath filePath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                // Download the file asynchronously
+                client.DownloadFile(webUri.Value, filePath.Value);
+                Console.WriteLine($"File downloaded successfully to {filePath}");
+            }
+            return Result.Success();
+        }
+    }
+}
