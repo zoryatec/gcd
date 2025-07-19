6221584db041636399e5bb6ad8e752b62763cc28
dawidwozny <92235733+dawidwozny@users.noreply.github.com>
2025-07-19 20:28:23 +0200

    install gcd as function

--- Install-Gcd.ps1
+++ Install-Gcd.ps1
@@ -1,27 +1,29 @@
 
-param(
-    [string]$NipmInstallerUri,
-    [string]$GcdFeed = "https://raw.githubusercontent.com/zoryatec/gcd/refs/heads/main/feed",
-    [string]$GcdPackageName = "gcd",
-    [string]$GcdVersion = "0.23.16"
-)
+function Install-Gcd {
+    param(
+        [string]$NipmInstallerUri,
+        [string]$GcdFeed = "https://raw.githubusercontent.com/zoryatec/gcd/refs/heads/main/feed",
+        [string]$GcdPackageName = "gcd",
+        [string]$GcdVersion = "0.23.16"
+    )
 
-$ErrorActionPreference = 'Stop'
+    $ErrorActionPreference = 'Stop'
 
-$exeUrl = "https://github.com/zoryatec/gcd/releases/download/$GcdVersion/gcd.exe"
-$exePath = "$env:TEMP\gcd.exe"
+    $exeUrl = "https://github.com/zoryatec/gcd/releases/download/$GcdVersion/gcd.exe"
+    $exePath = "$env:TEMP\gcd.exe"
 
-Write-Host "!!!!!!!!!!!!!!!!!! BOOTSTRAP GCD !!!!!!!!!!!!!!!!!!!!"
-try {
-    Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
-    & $exePath tools bootstrap --nipkg-installer-source-uri $NipmInstallerUri --gcd-feed $GcdFeed --gcd-package-name $GcdPackageName
-    $exitCode = $LASTEXITCODE
-    if ($exitCode -ne 0) {
-        exit $exitCode
-    }
-} finally {
-    if (Test-Path $exePath) {
-        Remove-Item $exePath -Force
+    Write-Host "!!!!!!!!!!!!!!!!!! BOOTSTRAP GCD !!!!!!!!!!!!!!!!!!!!"
+    try {
+        Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
+        & $exePath tools bootstrap --nipkg-installer-source-uri $NipmInstallerUri --gcd-feed $GcdFeed --gcd-package-name $GcdPackageName
+        $exitCode = $LASTEXITCODE
+        if ($exitCode -ne 0) {
+            exit $exitCode
+        }
+    } finally {
+        if (Test-Path $exePath) {
+            Remove-Item $exePath -Force
+        }
     }
 }
 
